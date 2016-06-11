ad8b57320074e31e647005a91d31262311726b90
Jack <jediguy9@gmail.com>
2016-06-11 12:57:50 -0400

    Uploaded files

---
+++ Assets/citiessightlines.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class citiessightlines : MonoBehaviour {
+
+    public ArrayList todelete;
+    void Start()
+    {
+        todelete = new ArrayList();
+    }
+    public int calcSight(string[]input)
+    {
+        todelete = new ArrayList();
+        int total = 0;
+        ArrayList check = new ArrayList();
+        for (int y=0;y < input.Length; y++)
+        {
+            char[] line = input[y].ToCharArray();
+            for (int x = 0; x < line.Length; x++)
+            {
+                char c = line[x];
+                if (c == '*')
+                {
+                    GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                    cube.transform.position = new Vector3(x, y);
+                    todelete.Add(cube);
+                }
+                if (c == '@')
+                {
+                    transform.position = new Vector3(x, y);
+                }
+            }
+        }
+        for (int angle=0; angle < 3600; angle++)
+        {
+            RaycastHit hit;
+            Physics.Raycast(transform.position, Quaternion.Euler(0, 0, angle/10) * Vector3.up, out hit);
+            if (hit.collider!=null)
+            {
+                GameObject hitObject = hit.collider.gameObject;
+                if (!check.Contains(hitObject)&&hitObject!=this)
+                {
+                    total += 1;
+                    check.Add(hitObject);
+                }
+           }
+        }
+        return total;
+    }
+}
---
+++ Assets/runner.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class runner : MonoBehaviour {
+
+    public citiessightlines test;
+    public Text output;
+    public void redoInput(string fulltext)
+    {
+        string[] input = fulltext.Split(new string[] { System.Environment.NewLine }, System.StringSplitOptions.None);
+        foreach(GameObject g in test.todelete)
+        {
+            Destroy(g);
+        }
+        output.text = test.calcSight(input).ToString();
+    }
+
+    void Update()
+    {
+        Vector2 mouseDelta = new Vector2(Input.GetAxisRaw("Mouse X") * 3, Input.GetAxisRaw("Mouse Y") * 3);
+        transform.RotateAround(transform.position, transform.right, -mouseDelta.y);
+        transform.RotateAround(transform.position, Vector3.forward, mouseDelta.x);
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+    }
+	
+}
